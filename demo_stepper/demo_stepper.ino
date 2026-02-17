faa7eb6f68098a4a7f4700285fe63d98fafa34b7
mike7442 <mikhail.popov.alexandrovich@gmail.com>
2026-02-17 17:59:45 +0300

    Update demo_stepper.ino

--- demo_stepper/demo_stepper.ino
+++ demo_stepper/demo_stepper.ino
@@ -3,9 +3,10 @@
 GStepper<STEPPER2WIRE> stepper(2048, 16, 17);
 
 void setup() {
-  Serial.begin(115200);
   pinMode(ENA, OUTPUT);
   stepper.setRunMode(FOLLOW_POS);
+  stepper.setAcceleration(600);
+  stepper.setMaxSpeed(2000);
   digitalWrite(ENA, 0);
 }
 
