ab528a666e743521444da81e73c3911be565abaa
mike7442 <mikhail.popov.alexandrovich@gmail.com>
2026-02-17 17:57:16 +0300

    Update demo_stepper.ino

--- demo_stepper/demo_stepper.ino
+++ demo_stepper/demo_stepper.ino
@@ -1,15 +1,18 @@
 #include <GyverStepper.h>
-GStepper<STEPPER2WIRE> stepper(2048, 14, 27);
+#define ENA 5
+GStepper<STEPPER2WIRE> stepper(2048, 16, 17);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
+  pinMode(ENA, OUTPUT);
   stepper.setRunMode(FOLLOW_POS);
-  digitalWrite(25, 0);
-  digitalWrite(26, 0);
+  digitalWrite(ENA, 0);
 }
 
 void loop() {
-    stepper.setTarget(1000);
+  if (!stepper.tick()) {
+    static bool dir;
+    dir = !dir;
+    stepper.setTarget(dir ? -1024 : 1024);
+  }
 }
